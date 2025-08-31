cd44828cec46d9cdfc23b7c0a00fb9dd7de173d4
FasterSpeeding <lucina@lmbyrne.dev>
2025-08-31 01:16:04 +0100

    initial containerfile definition (#1)

---
+++ build.bash
@@ -0,0 +1,30 @@
+set -ouex pipefail
+
+FX_CAST_VERSION="0.3.0"
+arch=$(uname -m)
+
+if [[ "$arch" == "aarch64" ]]; then
+    arch="arm64"
+elif [[ "$arch" == "x86_64" ]]; then
+    arch="x64"
+fi
+
+# Setup fx_cast bridge
+temp_dir=$(mktemp -d)
+pushd "$temp_dir"
+
+wget https://github.com/hensm/fx_cast/releases/download/v$FX_CAST_VERSION/fx_cast_bridge-$FX_CAST_VERSION-$arch.rpm -O ./fx_cast_bridge.rpm
+mkdir /var/opt
+dnf5 install -y ./fx_cast_bridge.rpm
+rm -rv /var/opt
+
+popd
+rm -rvf "$temp_dir"
+
+useradd --system fx_cast
+cp ./artifacts/fx_cast.service /etc/systemd/system/fx_cast.service
+systemctl enable fx_cast
+
+# Setup Waydroid
+
+dnf5 install -y waydroid
