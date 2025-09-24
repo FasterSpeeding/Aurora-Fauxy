2be434d530ab54fba5a2bb91677263ce4c7dde59
FasterSpeeding <lucina@lmbyrne.dev>
2025-09-24 20:09:11 +0100

    Resolve user-defined aliases for sudo calls

---
+++ artifacts/skel/.bashrc.d/aliases.bash
@@ -0,0 +1,5 @@
+# File synced from https://github.com/FasterSpeeding/Aurora-Fauxy
+# Changes will not be persisted
+
+# Sudo alias hack to allow sudo to call user-defined aliases.
+alias sudo='sudo '
