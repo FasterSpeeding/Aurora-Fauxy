28a03358898c6d4243e1ba546e775c08ee3ba6c2
FasterSpeeding <lucina@lmbyrne.dev>
2025-09-24 20:39:42 +0000

    Use xargs

--- artifacts/skel/.config/just/bash_scripts/sync_config_symlinks.bash
+++ artifacts/skel/.config/just/bash_scripts/sync_config_symlinks.bash
@@ -1,4 +1,1 @@
-while read -r -d $'\0' absolute; read -r -d $'\0' symlink
-do
-    ln -sf "$absolute" "$symlink"
-done < <(jq -c --raw-output0 '.links| map(.absolute, .symlink) | flatten | .[]' "$SYMLINK_TRACKER")
+jq -c --raw-output0 '.links[] | [.absolute, .symlink] | flatten | .[]' "$SYMLINK_TRACKER" | xargs -0 -n2 ln -fsv
