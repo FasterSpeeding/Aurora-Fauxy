d809dd3babe2dc3ecaff734b5a58bb9a6bfaf2c1
FasterSpeeding <lucina@lmbyrne.dev>
2025-09-26 18:38:43 +0100

    Fix sync symlink command

--- artifacts/skel/.config/just/bash_scripts/sync_config_symlinks.bash
+++ artifacts/skel/.config/just/bash_scripts/sync_config_symlinks.bash
@@ -1,2 +1,2 @@
 jq -c --raw-output0 '.dirs[]' "$SYMLINK_TRACKER" | xargs -0 -I {} mkdir -vp {}
-jq -c --raw-output0 '.links[] | [.absolute, .symlink] | flatten | .[]' "$SYMLINK_TRACKER" | xargs -0 -n2 ln -fsv
+jq -c --raw-output0 '.links[] | [.symlink, .absolute] | flatten | .[]' "$SYMLINK_TRACKER" | xargs -0 -n2 ln -fsv
