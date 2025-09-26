1e13f9aada8c30e2c58fec02c998cc42e872122f
FasterSpeeding <lucina@lmbyrne.dev>
2025-09-26 20:47:27 +0100

    Only sync skel symlinks

--- artifacts/skel/.config/just/bash_scripts/sync_config_symlinks.bash
+++ artifacts/skel/.config/just/bash_scripts/sync_config_symlinks.bash
@@ -1,2 +1,10 @@
-jq -c --raw-output0 '.dirs[]' "$SYMLINK_TRACKER" | xargs -0 -I {} mkdir -vp {}
-jq -c --raw-output0 '.links[] | [.symlink, .absolute] | flatten | .[]' "$SYMLINK_TRACKER" | xargs -0 -n2 ln -fsv
+while read -r -d $'\0' absolute; read -r -d $'\0' symlink
+do
+    if [[ "$absolute" == /etc/skel* ]]
+    then
+      sym_dir=$(dirname "$symlink")
+
+      mkdir -vp "$sym_dir"
+      ln -sf "$absolute" "$symlink"
+    fi
+done < <(jq -c --raw-output0 '.links[] | [.absolute, .symlink] | flatten | .[]' "$SYMLINK_TRACKER")
