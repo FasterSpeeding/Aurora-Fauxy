8141c4ca3f09cc4c588a12c2758c5dac3839291b
FasterSpeeding <lucina@lmbyrne.dev>
2025-09-27 10:42:28 +0100

    Fix ajust command (#79)

--- artifacts/bashrc/ajust.bash
+++ artifacts/bashrc/ajust.bash
@@ -1,4 +1,4 @@
 # File synced from https://github.com/FasterSpeeding/Aurora-Fauxy
 # Changes will not be persisted
 
-ajust() { JUST_JUSTFILE="$AURORA_ARTIFACTS/just/justfile" just ; }
+ajust() { JUST_JUSTFILE="$AURORA_ARTIFACTS/just/justfile" just "${@}"; }
