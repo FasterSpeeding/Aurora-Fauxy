086dbea7fe835851f7ad0c10351de5d95af7ff5a
FasterSpeeding <lucina@lmbyrne.dev>
2025-10-05 22:06:54 +0100

    move global bash commands to /usr/bin (#104)

--- artifacts/bashrc/startdev.bash
+++
@@ -1,9 +0,0 @@
-# File synced from https://github.com/FasterSpeeding/Aurora-Fauxy
-# Changes will not be persisted
-
-function startdev() {
-  local base=ghcr.io/fasterspeeding/devcontainer:master
-  local target=${1?Pass the container as the first argument}
-  shift 1
-  devpod up --git-clone-recursive-submodules --fallback-image "$base" "$target" "$@"
-}
