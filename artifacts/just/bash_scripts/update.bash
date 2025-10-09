aff8e30eb8db5a407848e6e9272a64bd43c52aae
FasterSpeeding <lucina@lmbyrne.dev>
2025-10-09 22:01:25 +0100

    Fix noop fwupdmgr get-updates calls in update task

--- artifacts/just/bash_scripts/update.bash
+++ artifacts/just/bash_scripts/update.bash
@@ -1,5 +1,16 @@
 mise upgrade --yes
 ujust update
 fwupdmgr refresh --force
-fwupdmgr get-updates
+
+exit_code=0
+fwupdmgr get-updates || exit_code=$?
+
+# fwupdmgr get-updates returns 2 for commands which passed without any action.
+if [[ "$exit_code" -qt 0 && "$exit_code" -ne 2 ]]
+then
+    echo "`fwupdmgr get-updates` exited with fail code $exit_code"
+    exit "$exit_code"
+fi
+
+
 fwupdmgr update
