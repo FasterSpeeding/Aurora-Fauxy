88f26486d96c70d7a07fa518ddee05d07cb94b64
FasterSpeeding <lucina@lmbyrne.dev>
2025-10-09 21:48:05 +0100

    Use cargo-binstall for mise cargo installs (#112)

--- artifacts/bashrc/env.bash
+++ artifacts/bashrc/env.bash
@@ -9,3 +9,6 @@ export EDITOR="$(command -v vi)"
 
 # Path to the file used to track the symlinks to sync for Aurora
 export SYMLINK_TRACKER="$AURORA_ARTIFACTS/.symlinks"
+
+# Disable telemetry in cargo-binstaller
+export BINSTALL_DISABLE_TELEMETRY="true"
