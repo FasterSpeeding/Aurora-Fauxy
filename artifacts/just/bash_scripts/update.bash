a0a166b7ab0c3e30e34534002cb096c4122ffb1f
FasterSpeeding <lucina@lmbyrne.dev>
2025-10-14 01:15:50 +0100

    update.bash fix

--- artifacts/just/bash_scripts/update.bash
+++ artifacts/just/bash_scripts/update.bash
@@ -6,7 +6,7 @@ exit_code=0
 fwupdmgr get-updates || exit_code=$?
 
 # fwupdmgr get-updates returns 2 for commands which passed without any action.
-if [[ "$exit_code" -qt 0 && "$exit_code" -ne 2 ]]
+if [[ "$exit_code" -gt 0 && "$exit_code" -ne 2 ]]
 then
     echo "`fwupdmgr get-updates` exited with fail code $exit_code"
     exit "$exit_code"
